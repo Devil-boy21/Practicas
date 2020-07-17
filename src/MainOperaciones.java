d78cf8d38c75e83916b42ba657bf2cffa367b6e5
SlimBoy <SlimBoy@RouterCNT>
2020-07-17 10:12:28 -0500

    esto es una prueba

---
+++ src/MainOperaciones.java
@@ -0,0 +1,12 @@
+
+public class MainOperaciones {
+    public static void main(String[] args) {
+        
+        
+         Division d= new Division(4,2);
+         double ;
+         
+    }
+    
+
+}
